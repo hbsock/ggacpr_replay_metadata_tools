24b371057c42761c0600587757de188fc176ea54
Hanbin Sock <hbsock@gmail.com>
2022-02-06 17:03:44 -0500

    Add function for parsing the date of the metadata.
    Also fixed the padding from 14 bytes to the correct 12 bytes. (I was
    consuming 2 extra bytes so was getting wrong values in my test)

--- parser.go
+++ parser.go
@@ -9,13 +9,25 @@ import (
 type ReplayHeader struct {
     Signature [12]byte
     Metadata_size uint16
-    _ [14]byte //To skip to the 0x1A byte where the metadata starts
+    _ [12]byte //To skip to the 0x1A byte where the metadata starts
+}
+
+type ReplayRecordingDate struct {
+    Year uint16
+    Month uint8
+    Day uint8
+    Hour uint8
+    Minute uint8
+    Second uint8
 }
 
 
 func GetReplayHeader(r io.Reader) (ReplayHeader, error) {
     header := ReplayHeader{}
-    binary.Read(r, binary.LittleEndian, &header )
+    err := binary.Read(r, binary.LittleEndian, &header )
+    if err != nil {
+        return header, err
+    }
 
     SIGNATURE := [12]byte{0x47, 0x47, 0x52, 0x02, 0x51, 0xad, 0xee, 0x77, 0x45, 0xd7, 0x48, 0xcd}
     if header.Signature != SIGNATURE {
@@ -28,3 +40,12 @@ func GetReplayHeader(r io.Reader) (ReplayHeader, error) {
 }
 
 
+func GetReplayRecordingDate(r io.Reader) (ReplayRecordingDate, error) {
+    recording_date := ReplayRecordingDate{}
+    err := binary.Read(r, binary.LittleEndian, &recording_date )
+    if err != nil {
+        return recording_date, err
+    }
+
+    return recording_date, nil
+}
--- parser_test.go
+++ parser_test.go
@@ -5,16 +5,16 @@ import (
     "os"
 )
 
+const TEST_FILE_PATH = "test_files/METADATA_ONLY_test_replay_20220127_1829_Klantsmurfen_RO_vs_Nibnab_JA.ggr"
+
 func TestGetReplayHeader(t *testing.T) {
     /*
      this test file has 136 bytes.
      12 bytes for the signature, 2 bytes for the metadata size
      then padded until 0x1A
      */
-    const test_file_path = "test_files/METADATA_ONLY_test_replay_20220127_1829_Klantsmurfen_RO_vs_Nibnab_JA.ggr"
 
-    file, _ := os.Open(test_file_path)
-
+    file, _ := os.Open(TEST_FILE_PATH)
     defer file.Close()
 
     _, err := GetReplayHeader(file)
@@ -23,3 +23,31 @@ func TestGetReplayHeader(t *testing.T) {
     }
 }
 
+
+func TestGetReplayRecordingDate(t *testing.T) {
+    file, _ := os.Open(TEST_FILE_PATH)
+    defer file.Close()
+
+    GetReplayHeader(file)
+
+    t.Log(file)
+
+    recording_date, err := GetReplayRecordingDate(file)
+    if err != nil {
+        t.Fatalf("%q", err)
+    }
+
+    expected_date := ReplayRecordingDate{
+        Year: 2022,
+        Month: 1,
+        Day: 27,
+        Hour: 18,
+        Minute: 29,
+        Second: 58,
+    }
+
+    if recording_date != expected_date {
+        t.Logf("Expected date was %+q but read %+q", expected_date, recording_date)
+        t.Fatalf("Expected date was %+v but read %+v", expected_date, recording_date)
+    }
+}
