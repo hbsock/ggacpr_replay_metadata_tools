b0e28d07b3cd0482ed5ba8d56ef625e8337f8dbf
Hanbin Sock <hbsock@gmail.com>
2022-02-07 11:05:11 -0500

    Run go fmt on the code

--- parser.go
+++ parser.go
@@ -1,122 +1,119 @@
 package parser
 
 import (
-    "io"
-    "encoding/binary"
-    "errors"
+	"encoding/binary"
+	"errors"
+	"io"
 )
 
 type ReplayHeader struct {
-    Signature [12]byte
-    Metadata_size uint16
-    _ [12]byte //To skip to the 0x1A byte where the metadata starts
+	Signature     [12]byte
+	Metadata_size uint16
+	_             [12]byte //To skip to the 0x1A byte where the metadata starts
 }
 
 type ReplayRecordingDate struct {
-    Year uint16
-    Month uint8
-    Day uint8
-    Hour uint8
-    Minute uint8
-    Second uint8
-    _ [1]byte //To skip to the 0x22 byte
+	Year   uint16
+	Month  uint8
+	Day    uint8
+	Hour   uint8
+	Minute uint8
+	Second uint8
+	_      [1]byte //To skip to the 0x22 byte
 }
 
 type Character uint8
+
 const (
-    SO Character = iota + 1
-    KY
-    MA
-    MI
-    AX
-    PO
-    CH
-    ED
-    BA
-    FA
-    TE
-    JA
-    AN
-    JO
-    VE
-    DI
-    SL
-    IN
-    ZA
-    BR
-    RO
-    AB
-    OS
-    KL
-    JU
+	SO Character = iota + 1
+	KY
+	MA
+	MI
+	AX
+	PO
+	CH
+	ED
+	BA
+	FA
+	TE
+	JA
+	AN
+	JO
+	VE
+	DI
+	SL
+	IN
+	ZA
+	BR
+	RO
+	AB
+	OS
+	KL
+	JU
 )
 
 type ReplayMetaDataContent struct {
-    P1SteamID uint64
-    P2SteamID uint64
-    P1NameUTF8 [32]byte
-    P2NameUTF8 [32]byte
-    P1Character Character
-    P2Character Character
-    ExtraFlag uint8 // value is 1 if extra options were modified, or EX/SP/GG characters were used, or KL/JU in the original AC, 0 otherwise
-    SingleOrTeamFlag uint8 // 1 = single, 2 = team
-    IsPlusRFlag uint8 // 0 = +R, 1 = AC
-    TimezoneBiasInSec int32 //
-    P1RoundsWon uint8
-    P2RoundsWon uint8
-    ProblemBitmask uint8 // 0b1 = unfinished match 0b10 = disconnect 0b100 = desync
-    Ping uint8
-    MatchDurationInFrames uint32
-    P1Score uint8
-    P2Score uint8
-    P1Rank uint8
-    P2Rank uint8
-    WinnerSide uint8 // 1 = P1, 2 = P2, 3 = draw
+	P1SteamID             uint64
+	P2SteamID             uint64
+	P1NameUTF8            [32]byte
+	P2NameUTF8            [32]byte
+	P1Character           Character
+	P2Character           Character
+	ExtraFlag             uint8 // value is 1 if extra options were modified, or EX/SP/GG characters were used, or KL/JU in the original AC, 0 otherwise
+	SingleOrTeamFlag      uint8 // 1 = single, 2 = team
+	IsPlusRFlag           uint8 // 0 = +R, 1 = AC
+	TimezoneBiasInSec     int32 //
+	P1RoundsWon           uint8
+	P2RoundsWon           uint8
+	ProblemBitmask        uint8 // 0b1 = unfinished match 0b10 = disconnect 0b100 = desync
+	Ping                  uint8
+	MatchDurationInFrames uint32
+	P1Score               uint8
+	P2Score               uint8
+	P1Rank                uint8
+	P2Rank                uint8
+	WinnerSide            uint8 // 1 = P1, 2 = P2, 3 = draw
 }
-
 
 type ReplayMetaData struct {
-    Header ReplayHeader
-    Date ReplayRecordingDate
-    Content ReplayMetaDataContent
+	Header  ReplayHeader
+	Date    ReplayRecordingDate
+	Content ReplayMetaDataContent
 }
-
 
 func GetReplayHeader(r io.Reader) (ReplayHeader, error) {
-    header := ReplayHeader{}
-    err := binary.Read(r, binary.LittleEndian, &header )
-    if err != nil {
-        return header, err
-    }
+	header := ReplayHeader{}
+	err := binary.Read(r, binary.LittleEndian, &header)
+	if err != nil {
+		return header, err
+	}
 
-    SIGNATURE := [12]byte{0x47, 0x47, 0x52, 0x02, 0x51, 0xad, 0xee, 0x77, 0x45, 0xd7, 0x48, 0xcd}
-    if header.Signature != SIGNATURE {
-        return header, errors.New("The file format signature is not a valid GG AC+R replay file")
-    } else if header.Metadata_size != 110 {
-        return header, errors.New("The metadata size is not 110, this replay file is likely not from the latest version of GG AC+R. Please watch this replay file until the end on the latest game version to update the metadata.")
-    }
+	SIGNATURE := [12]byte{0x47, 0x47, 0x52, 0x02, 0x51, 0xad, 0xee, 0x77, 0x45, 0xd7, 0x48, 0xcd}
+	if header.Signature != SIGNATURE {
+		return header, errors.New("The file format signature is not a valid GG AC+R replay file")
+	} else if header.Metadata_size != 110 {
+		return header, errors.New("The metadata size is not 110, this replay file is likely not from the latest version of GG AC+R. Please watch this replay file until the end on the latest game version to update the metadata.")
+	}
 
-    return header, nil
+	return header, nil
 }
 
-
 func GetReplayRecordingDate(r io.Reader) (ReplayRecordingDate, error) {
-    recording_date := ReplayRecordingDate{}
-    err := binary.Read(r, binary.LittleEndian, &recording_date )
-    if err != nil {
-        return recording_date, err
-    }
+	recording_date := ReplayRecordingDate{}
+	err := binary.Read(r, binary.LittleEndian, &recording_date)
+	if err != nil {
+		return recording_date, err
+	}
 
-    return recording_date, nil
+	return recording_date, nil
 }
 
 func GetReplayMetaData(r io.Reader) (ReplayMetaData, error) {
-    metadata := ReplayMetaData{}
-    err := binary.Read(r, binary.LittleEndian, &metadata)
-    if err != nil {
-        return metadata, err
-    }
+	metadata := ReplayMetaData{}
+	err := binary.Read(r, binary.LittleEndian, &metadata)
+	if err != nil {
+		return metadata, err
+	}
 
-    return metadata, nil
+	return metadata, nil
 }
-
--- parser_test.go
+++ parser_test.go
@@ -1,91 +1,89 @@
 package parser
 
 import (
-    "testing"
-    "os"
+	"os"
+	"testing"
 )
 
 const TEST_FILE_PATH = "test_files/METADATA_ONLY_test_replay_20220127_1829_Klantsmurfen_RO_vs_Nibnab_JA.ggr"
 
 func TestGetReplayHeader(t *testing.T) {
-    /*
-     this test file has 136 bytes.
-     12 bytes for the signature, 2 bytes for the metadata size
-     then padded until 0x1A
-     */
+	/*
+	   this test file has 136 bytes.
+	   12 bytes for the signature, 2 bytes for the metadata size
+	   then padded until 0x1A
+	*/
 
-    file, _ := os.Open(TEST_FILE_PATH)
-    defer file.Close()
+	file, _ := os.Open(TEST_FILE_PATH)
+	defer file.Close()
 
-    _, err := GetReplayHeader(file)
-    if err != nil {
-        t.Fatalf("%q", err)
-    }
+	_, err := GetReplayHeader(file)
+	if err != nil {
+		t.Fatalf("%q", err)
+	}
 }
-
 
 func TestGetReplayRecordingDate(t *testing.T) {
-    file, _ := os.Open(TEST_FILE_PATH)
-    defer file.Close()
+	file, _ := os.Open(TEST_FILE_PATH)
+	defer file.Close()
 
-    GetReplayHeader(file)
+	GetReplayHeader(file)
 
-    recording_date, err := GetReplayRecordingDate(file)
-    if err != nil {
-        t.Fatalf("%q", err)
-    }
+	recording_date, err := GetReplayRecordingDate(file)
+	if err != nil {
+		t.Fatalf("%q", err)
+	}
 
-    expected_date := ReplayRecordingDate{
-        Year: 2022,
-        Month: 1,
-        Day: 27,
-        Hour: 18,
-        Minute: 29,
-        Second: 58,
-    }
+	expected_date := ReplayRecordingDate{
+		Year:   2022,
+		Month:  1,
+		Day:    27,
+		Hour:   18,
+		Minute: 29,
+		Second: 58,
+	}
 
-    if recording_date != expected_date {
-        t.Logf("Expected date was %+q but read %+q", expected_date, recording_date)
-        t.Fatalf("Expected date was %+v but read %+v", expected_date, recording_date)
-    }
+	if recording_date != expected_date {
+		t.Logf("Expected date was %+q but read %+q", expected_date, recording_date)
+		t.Fatalf("Expected date was %+v but read %+v", expected_date, recording_date)
+	}
 }
 
-
 func TestGetReplayMetaData(t *testing.T) {
-    file, _ := os.Open(TEST_FILE_PATH)
-    defer file.Close()
+	file, _ := os.Open(TEST_FILE_PATH)
+	defer file.Close()
 
-    metadata, err := GetReplayMetaData(file)
-    if err != nil {
-        t.Fatalf("%q", err)
-    }
+	metadata, err := GetReplayMetaData(file)
+	if err != nil {
+		t.Fatalf("%q", err)
+	}
 
-    expected_content := ReplayMetaDataContent{
-        P1SteamID: 76561198008085514,
-        P2SteamID: 76561198011058687,
-        P1Character: RO,
-        P2Character: JA,
-        ExtraFlag: 0,
-        SingleOrTeamFlag: 1,
-        IsPlusRFlag: 0,
-        TimezoneBiasInSec: 18000,
-        P1RoundsWon: 2,
-        P2RoundsWon: 0,
-        ProblemBitmask: 0,
-        Ping: 124,
-        MatchDurationInFrames: 6810,
-        P1Score: 4,
-        P2Score: 0,
-        P1Rank: 5,
-        P2Rank: 3,
-        WinnerSide: 1,
-    }
-    copy(expected_content.P1NameUTF8[:], []byte("Klantsmurfen"))
-    copy(expected_content.P2NameUTF8[:], []byte("Nibnab"))
+	expected_content := ReplayMetaDataContent{
+		P1SteamID:             76561198008085514,
+		P2SteamID:             76561198011058687,
+		P1Character:           RO,
+		P2Character:           JA,
+		ExtraFlag:             0,
+		SingleOrTeamFlag:      1,
+		IsPlusRFlag:           0,
+		TimezoneBiasInSec:     18000,
+		P1RoundsWon:           2,
+		P2RoundsWon:           0,
+		ProblemBitmask:        0,
+		Ping:                  124,
+		MatchDurationInFrames: 6810,
+		P1Score:               4,
+		P2Score:               0,
+		P1Rank:                5,
+		P2Rank:                3,
+		WinnerSide:            1,
+	}
+	copy(expected_content.P1NameUTF8[:], []byte("Klantsmurfen"))
+	copy(expected_content.P2NameUTF8[:], []byte("Nibnab"))
 
-    //t.Logf("Sol value is %d", SO)
+	//t.Logf("Sol value is %d", SO)
 
-    if expected_content != metadata.Content {
-        t.Fatalf("Expected content %v did not match the actual content %v", expected_content, metadata.Content)
-    }
+	if expected_content != metadata.Content {
+		t.Fatalf("Expected content %v did not match the actual content %v", expected_content, metadata.Content)
+	}
 }
